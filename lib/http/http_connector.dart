8b5625b47b601e3394a63fa4c5e783f7d2ea4914
gmacgre <gmacgre@byu.edu>
2025-01-05 11:00:24 -0600

    Change connection to server to be via websockets

--- lib/http/http_connector.dart
+++ lib/http/http_connector.dart
@@ -1,15 +1,38 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:twilight_imperium/shared/http/login/login_request.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class HTTPConnector {
-  static const String _host = 'http://localhost:3000';
+  static const String _host = 'ws://localhost:3000';
+  static WebSocketChannel? _connection;
 
   static Future<void> login(LoginRequest request) async {
-    http.post(Uri.parse('$_host/api/login'), body: jsonEncode(request.toJson()));
+    WebSocketChannel channel = await _getConnection();
+    channel.sink.add(jsonEncode(request.toJson()));
   }
-
+  
   static Future<void> createGame() async {
 
+  }
+
+  static Future<WebSocketChannel> _getConnection() async {
+    if(_connection == null) {
+        _connection = WebSocketChannel.connect(Uri.parse(_host));
+        await _connection!.ready;
+        _connection!.stream.listen(_handleMessage);
+    }
+    return _connection!;
+  }
+
+  static void _handleMessage(dynamic message) {
+    print('\'ello Govnah!');
+    print(message.runtimeType);
+    print(message);
+  }
+
+  static void closeSocket() async {
+    WebSocketChannel channel = await _getConnection();
+    channel.sink.close();
+    _connection = null;
   }
 }
